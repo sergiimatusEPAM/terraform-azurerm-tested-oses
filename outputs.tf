c93c8a632c3bffb9a1a18dd222a3b04c20e8fc79
Lukas Loesche <lukas@mesosphere.io>
2018-09-27 16:55:59 +0200

    Automatic update of variable descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,31 @@
 # Output
 output "user" {
+  description = "user"
   value = "${data.template_file.traditional_os_user.rendered}"
 }
 
 output "azure_offer" {
+  description = "azure offer"
   value = "${data.template_file.azure_offer.rendered}"
 }
 
 output "azure_publisher" {
+  description = "azure publisher"
   value = "${data.template_file.azure_publisher.rendered}"
 }
 
 output "azure_sku" {
+  description = "azure sku"
   value = "${data.template_file.azure_sku.rendered}"
 }
 
 output "azure_version" {
+  description = "azure version"
   value = "${data.template_file.azure_version.rendered}"
 }
 
 # Main Output
 output "os-setup" {
+  description = "os-setup"
   value = "${data.template_file.os-setup.rendered}"
 }
